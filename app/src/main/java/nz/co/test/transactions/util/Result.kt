30535fe0a185f216d381dd422c369339995821b7
Alex <Jokejiao@gmail.com>
2025-03-07 22:31:55 +1300

    Introduce `Result` sealed interface to model data loading states

---
+++ app/src/main/java/nz/co/test/transactions/util/Result.kt
@@ -0,0 +1,8 @@
+package nz.co.test.transactions.util
+
+
+sealed interface Result<out T> {
+    data class Success<T>(val data: T) : Result<T>
+    data class Error(val exception: Throwable) : Result<Nothing>
+    data object Loading : Result<Nothing>
+}
