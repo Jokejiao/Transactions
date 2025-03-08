b7172c6039441bfaa949c58ef2eff23a23aa1880
Alex <Jokejiao@gmail.com>
2025-03-09 11:11:44 +1300

    Remove unnecessary parameters for TransactionNavGraph

--- app/src/main/java/nz/co/test/transactions/TransactionNavGraph.kt
+++ app/src/main/java/nz/co/test/transactions/TransactionNavGraph.kt
@@ -1,16 +1,12 @@
 package nz.co.test.transactions
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import kotlinx.coroutines.CoroutineScope
 import nz.co.test.transactions.transactiondetail.TransactionDetailScreen
 import nz.co.test.transactions.transactions.TransactionsScreen
 
@@ -18,15 +14,11 @@ import nz.co.test.transactions.transactions.TransactionsScreen
 fun TransactionNavGraph(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
-    coroutineScope: CoroutineScope = rememberCoroutineScope(),
     startDestination: String = TransactionDestinations.TRANSACTIONS_ROUTE,
     navActions: TransactionNavigationActions = remember(navController) {
         TransactionNavigationActions(navController)
     }
 ) {
-    val currentNavBackStackEntry by navController.currentBackStackEntryAsState()
-    val currentRoute = currentNavBackStackEntry?.destination?.route ?: startDestination
-
     NavHost(
         navController = navController,
         startDestination = startDestination,
