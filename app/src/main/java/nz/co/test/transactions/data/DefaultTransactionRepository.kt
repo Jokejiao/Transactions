a8695969db0e1d41e94f58aa0c8d37717be30b64
Alex <Jokejiao@gmail.com>
2025-03-08 16:58:23 +1300

    Refactor the DefaultTransactionRepository.kt

--- app/src/main/java/nz/co/test/transactions/data/DefaultTransactionRepository.kt
+++ app/src/main/java/nz/co/test/transactions/data/DefaultTransactionRepository.kt
@@ -6,31 +6,36 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 import nz.co.test.transactions.data.source.local.TransactionDao
-import nz.co.test.transactions.data.source.network.TransactionNetworkDataSource
+import nz.co.test.transactions.data.source.network.NetworkDataSource
 import nz.co.test.transactions.di.DefaultDispatcher
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class DefaultTransactionRepository @Inject constructor(
-    private val networkDataSource: TransactionNetworkDataSource,
+    private val networkDataSource: NetworkDataSource,
     private val localDataSource: TransactionDao,
     @DefaultDispatcher private val dispatcher: CoroutineDispatcher,
 ) : TransactionRepository {
 
     override fun getTransactionsStream(): Flow<List<Transaction>> {
-        return localDataSource.observeAll().map { tasks ->
+        return localDataSource.observeAll().map { transactions ->
             withContext(dispatcher) {
-                tasks.toExternal()
+                transactions.toExternal()
             }
         }
     }
 
-    override suspend fun refresh() {
-        withContext(dispatcher) {
-            val remoteTransactions = networkDataSource.loadTransactions()
-            localDataSource.deleteAll()
-            localDataSource.upsertAll(remoteTransactions.toLocal())
+    override suspend fun refresh(): Boolean {
+        return try {
+            withContext(dispatcher) {
+                val remoteTransactions = networkDataSource.loadTransactions()
+                localDataSource.deleteAll()
+                localDataSource.upsertAll(remoteTransactions.toLocal())
+            }
+            true
+        } catch (e: Exception) {
+            false
         }
     }
 }
