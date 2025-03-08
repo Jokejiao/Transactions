7d0df3ae777d6013eef6a8940962386f374eb361
Alex <Jokejiao@gmail.com>
2025-03-08 17:13:28 +1300

    Create ComposeUtils.kt for the Loading content

---
+++ app/src/main/java/nz/co/test/transactions/util/ComposeUtils.kt
@@ -0,0 +1,28 @@
+package nz.co.test.transactions.util
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+
+
+@Composable
+fun LoadingContent(
+    loading: Boolean,
+    empty: Boolean,
+    emptyContent: @Composable () -> Unit,
+    onRefresh: () -> Unit,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    if (empty) {
+        emptyContent()
+    } else {
+        SwipeRefresh(
+            state = rememberSwipeRefreshState(loading),
+            onRefresh = onRefresh,
+            modifier = modifier,
+            content = content,
+        )
+    }
+}
